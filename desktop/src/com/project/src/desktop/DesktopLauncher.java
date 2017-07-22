47954505bdbf1b490720514aeba80b78b793efd8
Kenneth fang <kwf37@cornell.edu>
2017-07-22 02:08:56 -0400

    First Commit

---
+++ desktop/src/com/project/src/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.project.src.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.project.src.Platformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Platformer(), config);
+	}
+}
